89dddf8c062d1a19063ed81c0bf70448eb2fb890
Nepey <31688202+Nepey@users.noreply.github.com>
2017-11-14 23:55:40 +0300

    Add files via upload

---
+++ swapArray2.js
@@ -0,0 +1,22 @@
+'use strict';
+(function swapArray(a) {
+    
+    var center = Math.floor(a.length/2);
+    for (var i=0; i< Math.floor(center/2); i++) {
+        var nextIndex = center - i - 1;
+        var x =a[i];
+        a[i]=a[nextIndex];
+        a[nextIndex] = x;
+    }
+    for (var i=(center+1), j=1; i< ((Math.floor(a.length*2/3)+1)); i++) {
+        var nextIndex = a.length - j;   
+        var x =a[i];
+        a[i]=a[nextIndex];
+        a[nextIndex] = x; 
+        j=j+1;
+        
+    }
+    console.log("[" + a.toString() + "]");
+}) ([1,2,3,4,5,6,7,8,9,10,11,12]);
+
+
